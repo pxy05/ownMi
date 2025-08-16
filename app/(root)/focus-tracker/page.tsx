c2fb837f327bf9ba2d1bd42bae630425d2fc2ca8
pxy05 <psky0695@gmail.com>
2025-08-16 18:12:08 +0100

    feat: fully working websocket connection and session vericication implemented

--- app/(root)/focus-tracker/page.tsx
+++ app/(root)/focus-tracker/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 import { useAuth } from "@/lib/auth-context";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardTitle, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { clear } from "console";
 
 const WS_URL = process.env.NEXT_PUBLIC_WS_URL;
 
@@ -10,11 +11,18 @@ const page = () => {
   // websocket logic -------------
   const [socketConnected, setSocketConnected] = useState(false);
   const [sessionConnected, setSessionConnected] = useState(false);
+  const sessionConnectedRef = useRef(false);
+
   const wsRef = useRef<WebSocket | null>(null);
+  const ws = wsRef.current;
   // user logic -----------------
   const { user, session } = useAuth();
   const token = session?.access_token || "";
   // timer logic -----------------
+
+  useEffect(() => {
+    sessionConnectedRef.current = sessionConnected;
+  }, [sessionConnected]);
 
   useEffect(() => {
     if (!token) return;
@@ -35,9 +43,20 @@ const page = () => {
     };
 
     ws.onmessage = (event) => {
-      switch (event.data.type) {
+      const data = JSON.parse(event.data);
+      console.log("Received WebSocket message:", data.type);
+      switch (data.type) {
         case "sessionExists":
+          console.log("Session exists for user:", user?.id);
           setSessionConnected(true);
+          break;
+        case "noSessionExists":
+          console.log("No active session for user:", user?.id);
+          setSessionConnected(false);
+          if (ws.readyState === WebSocket.OPEN && !sessionConnected) {
+            console.log("Sending Session Request...");
+            ws.send(JSON.stringify({ type: "create-session" }));
+          }
           break;
         default:
           break;
@@ -45,27 +64,51 @@ const page = () => {
     };
 
     const heartbeat = setInterval(() => {
-      console.log("Sending heartbeat...");
-      if (ws.readyState === WebSocket.OPEN && sessionConnected) {
+      if (ws.readyState === WebSocket.OPEN && sessionConnectedRef.current) {
+        console.log("Sending heartbeat...");
         ws.send(JSON.stringify({ type: "heartbeat" }));
       }
     }, 10000);
 
     const sessionCheck = setInterval(() => {
-      console.log("Checking session status...");
-      if (ws.readyState === WebSocket.OPEN && !sessionConnected) {
+      if (ws.readyState === WebSocket.OPEN && !sessionConnectedRef.current) {
+        console.log("Checking session status...");
         ws.send(JSON.stringify({ type: "sessionCheck" }));
       }
     }, 500);
+
+    return () => {
+      clearInterval(sessionCheck);
+      clearInterval(heartbeat);
+      if (ws.readyState === WebSocket.OPEN && sessionConnectedRef.current) {
+        console.log("Ending session...");
+        ws.send(JSON.stringify({ type: "end-session" }));
+      }
+      ws.close();
+    };
   }, [token]);
 
   return (
     <div className="flex">
       <div className="flex-1">
-        WebSocket Status: {socketConnected ? "Connected" : "Disconnected"}
+        <Card className="text-center">
+          <CardHeader>WebSocket Status</CardHeader>
+          <CardContent
+            className={socketConnected ? "text-green-500" : "text-red-500"}
+          >
+            {socketConnected ? "Connected" : "Disconnected"}
+          </CardContent>
+        </Card>
       </div>
       <div className="flex-1">
-        {sessionConnected ? "Session Active" : "No Active Session"}
+        <Card className="text-center">
+          <CardHeader>Session Status</CardHeader>
+          <CardContent
+            className={sessionConnected ? "text-green-500" : "text-red-500"}
+          >
+            {sessionConnected ? "Session Active" : "No Active Session"}
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
